1d92a364bf0795383362f59fd7c03e6d6bb8b8c0
Konan Houphoue <bahobab@hotmail.com>
2021-03-16 15:27:26 -0500

    course/admin user: add, delete operations

--- cypress/components/course_component.js
+++ cypress/components/course_component.js
@@ -1,1 +1,8 @@
 export const deleteCourseButton = () => cy.get('[data-cy=delete-course-button]');
+export const addCourseInput = () => cy.get('[data-cy=new-course-input]');
+export const addCourseButton = () => cy.get('[data-cy=new-course-button]');
+
+export const addNewCourse = (courseName) => {
+  addCourseInput().type(courseName);
+  addCourseButton().click();
+}
--- cypress/e2e/courses.spec.js
+++ cypress/e2e/courses.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-import * as LogingComponent from '../components/login_component';
+// import * as LogingComponent from '../components/login_component';
 import * as CourseComponent from '../components/course_component';
 import * as Utils from '../support/utils';
 
@@ -15,5 +15,18 @@ describe('Course functionalities', () => {
     it('should show the delete course button', () => {
       CourseComponent.deleteCourseButton().should('be.visible');
     });
+
+    it('add a new course', () => {
+      CourseComponent.addNewCourse('Learn Lua');
+      CourseComponent.deleteCourseButton().should('have.length', 3);
+      
+    });
+
+    it('should delete an existing course', () => {
+      CourseComponent.deleteCourseButton().last().click();
+      CourseComponent.deleteCourseButton().should('have.length', 2);
+    });
+
   });
-})
+
+});
