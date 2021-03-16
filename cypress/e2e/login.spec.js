82a352ebf7552e0230f83d7bc8d5b5be2af6e0f5
Konan Houphoue <bahobab@hotmail.com>
2021-03-16 11:23:12 -0500

    cypress experimentalStudion

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -10,4 +10,15 @@ describe('test all login functionalities', () => {
     cy.get('[data-cy=login-password-field]').type('user');
     cy.get('[data-cy=login-button]').click();
   });
+
+  /* === Test Created with Cypress Studio === */
+  it('admin user should login with email and password', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[data-cy=login-email-field]').clear();
+    cy.get('[data-cy=login-email-field]').type('admin');
+    cy.get('[data-cy=login-password-field]').clear();
+    cy.get('[data-cy=login-password-field]').type('admin');
+    cy.get('[data-cy=login-button]').click();
+    /* ==== End Cypress Studio ==== */
+  });
 })
